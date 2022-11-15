7037aa9a69454e4155d5f211cf2e2562f3574609
[Qamardeen] <56746380+Qamardeen@users.noreply.github.com>
2022-11-15 13:27:47 -0500

    added player sprites and movement
    requires playtesting please check out how player feels

---
+++ DinoJam/Assets/Scriptes/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;
+
+    private Rigidbody2D rb;
+    private Vector3 currMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal"); 
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        currMovement = new Vector3(inputX, inputY, playerSpeed);
+
+        currMovement = currMovement * playerSpeed * Time.deltaTime;
+
+        transform.position += currMovement;
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        
+        
+    }
+}
