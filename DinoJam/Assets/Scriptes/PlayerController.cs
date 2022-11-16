b11bd5c8f79e2088288bd2f3b8af2cddab1e22bb
[Qamardeen] <56746380+Qamardeen@users.noreply.github.com>
2022-11-15 21:28:14 -0500

    added new player model and animations
    some bugs may exist

--- DinoJam/Assets/Scriptes/PlayerController.cs
+++ DinoJam/Assets/Scriptes/PlayerController.cs
@@ -4,35 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
+
     public float playerSpeed;
 
-    private Rigidbody2D rb;
     private Vector3 currMovement;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxisRaw("Horizontal"); 
+        float inputX = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", inputX);
+
         float inputY = Input.GetAxisRaw("Vertical");
 
         currMovement = new Vector3(inputX, inputY, 0);
-
         currMovement = currMovement * playerSpeed * Time.deltaTime;
-
         transform.position += currMovement;
-    }
-
-
-
-    private void FixedUpdate()
-    {
-        
-        
     }
 }
