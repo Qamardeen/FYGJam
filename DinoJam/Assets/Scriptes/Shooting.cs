f52b1a5d362e74e73648b43888f4c1866aa4497f
[Qamardeen] <56746380+Qamardeen@users.noreply.github.com>
2022-11-27 23:38:48 -0500

    Added enemy hp

--- DinoJam/Assets/Scriptes/Shooting.cs
+++ DinoJam/Assets/Scriptes/Shooting.cs
@@ -8,26 +8,39 @@ public class Shooting : MonoBehaviour
     public Transform FirePoint;
     public GameObject bulletPrefab;
 
-    public float bulletForce = 20f;
+    public float fireRate;
+
+    private bool canShoot;
+
+    //public float bulletForce = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        canShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canShoot)
         {
+            
             Shoot();
         }
     }
+
+    IEnumerator ShotCoolDown()
+    {
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
+    }
     void Shoot()
     {
+        canShoot = false;
         GameObject bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
+        StartCoroutine(ShotCoolDown());
+        //Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        //rb.AddForce(FirePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 }
